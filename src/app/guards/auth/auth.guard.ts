00b7dc84391ebdd8c6197da4e1976eaafd0b356c
GringoXY <przemyslawkowalski01pk@gmail.com>
2024-04-24 20:48:36 +0200

    add auth guard

---
+++ src/app/guards/auth/auth.guard.ts
@@ -0,0 +1,20 @@
+import { inject } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivateFn, RouterStateSnapshot } from '@angular/router';
+import { AuthService } from '../services/auth/auth.service';
+import { RouterExtendedService } from '../services/router-extended/router-extended.service';
+
+export const authGuard: CanActivateFn = (route, state) => {
+  return handleAuth(route, state);
+};
+
+function handleAuth(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+  const auth = inject(AuthService);
+  const routerExtended = inject(RouterExtendedService);
+
+  if (auth.isAuthenticated) {
+    return true;
+  }
+
+  routerExtended.navigateToSignIn(state.url);
+  return false;
+}
