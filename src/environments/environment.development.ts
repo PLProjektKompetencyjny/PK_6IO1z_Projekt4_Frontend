bd5f75a37c02252f15fb32661f34e3e2aef94a2a
GringoXY <przemyslawkowalski01pk@gmail.com>
2024-04-24 19:26:07 +0200

    add environments files

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  domain: 'localhost',
+  apiUrl: 'localhost:5000/api'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  domain: 'travelnest.com',
+  apiUrl: 'travelnest.com:1000/api'
+};
