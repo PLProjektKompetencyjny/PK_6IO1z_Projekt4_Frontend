94007fd4684692a91b7f90a49e7981a8108a0348
GringoXY <przemyslawkowalski01pk@gmail.com>
2024-05-02 11:30:32 +0200

    secure profile related paths

--- src/app/modules/profile/profile.routes.ts
+++ src/app/modules/profile/profile.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { ProfileComponent } from './components/profile/profile.component';
 import { DetailsComponent } from './components/details/details.component';
 import { SecurityComponent } from './components/security/security.component';
+import { authGuard } from '../../guards/auth/auth.guard';
 
 export const routes: Routes = [
   {
     path: '',
     component: ProfileComponent,
+    canActivate: [authGuard],
     children: [
       {
         path: 'details',
