7ec894c6b13c357d55d3c890baf74816e717d9dd
Peter <62666278+GringoXY@users.noreply.github.com>
2024-05-21 20:14:13 +0200

    add sign in view (#37)

---
+++ src/app/modules/sign-in/components/sign-in/sign-in.component.ts
@@ -0,0 +1,68 @@
+import { Component } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../../../services/auth/auth.service';
+import { appName } from './sign-in.config';
+
+@Component({
+  selector: 'tn-sign-in',
+  templateUrl: './sign-in.component.html',
+  styleUrl: './sign-in.component.scss'
+})
+export class SignInComponent {
+
+  protected readonly appName = appName;
+
+  form: FormGroup;
+
+  /**
+   * An error message to display.
+   * For each step error message is different.
+   */
+  errorMessage: string = '';
+
+  get email(): AbstractControl<string, string> | null {
+    return this.form.get('email');
+  }
+
+  get password(): AbstractControl<string, string> | null {
+    return this.form.get('password');
+  }
+
+  constructor(
+    private readonly formBuilder: FormBuilder,
+    private readonly authService: AuthService,
+  ) {
+    this.form = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
+
+  async submit(): Promise<void> {
+    if (this.validateEmail() === false) {
+      this.errorMessage = 'Enter valid email address';
+      return;
+    }
+
+    if (this.validatePassword() === false) {
+      this.errorMessage = 'Enter password';
+      return;
+    }
+
+    this.errorMessage = '';
+
+    try {
+      await this.authService.signIn(this.email?.value ?? '', this.password?.value ?? '');
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  validateEmail(): boolean {
+    return this.email?.valid ?? false;
+  }
+
+  validatePassword(): boolean {
+    return this.password?.valid ?? false;
+  }
+}
