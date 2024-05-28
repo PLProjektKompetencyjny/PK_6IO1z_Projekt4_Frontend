24f0d56572ee57ce923e214ea10205d3bb38d0cc
GringoXY <przemyslawkowalski01pk@gmail.com>
2024-05-28 21:20:57 +0200

    fix virtual tour

--- src/app/modules/virtual-tour/components/virtual-tour/virtual-tour.component.ts
+++ src/app/modules/virtual-tour/components/virtual-tour/virtual-tour.component.ts
@@ -1,5 +1,7 @@
-import { AfterViewInit, Component } from '@angular/core';
-import { appName, startRoomNo } from './virtual-tour.config';
+import { AfterViewInit, Component, ElementRef, HostListener, Renderer2, ViewChild } from '@angular/core';
+import { appName, assets, baseAssetsPath, enableDamping, maxRooms, maxZoom, minZoom, rotateSpeed, startRoomNo, startZoom } from './virtual-tour.config';
+import * as THREE from 'three';
+import { OrbitControls } from 'three-orbitcontrols-ts';
 
 @Component({
   selector: 'tn-virtual-tour',
@@ -8,27 +10,77 @@ import { appName, startRoomNo } from './virtual-tour.config';
 })
 export class VirtualTourComponent implements AfterViewInit {
 
+  @ViewChild('container') container!: ElementRef<HTMLDivElement>;
+
   protected readonly appName = appName;
-  currentRoom = startRoomNo;
+  protected readonly assets = assets;
+  currentRoomNo: number = startRoomNo;
+  currentZoom: number = startZoom;
+  generatingScene: boolean = true;
+
+  renderer = new THREE.WebGLRenderer();
+  scene: THREE.Scene = new THREE.Scene();
+  camera: THREE.PerspectiveCamera = new THREE.PerspectiveCamera(100, window.innerWidth / window.innerHeight, 0.1, 1000);
+  mesh: THREE.Mesh = new THREE.Mesh();
+  geometry: THREE.SphereGeometry = new THREE.SphereGeometry(100, 400, 1000).scale(-1, 1, 1);
+  controls: OrbitControls = new OrbitControls(this.camera, this.renderer.domElement);
+  loader: THREE.TextureLoader = new THREE.TextureLoader();
+
+  constructor(private readonly renderer2: Renderer2) {
+    this.camera.position.z = 1;
+    this.controls.rotateSpeed *= -rotateSpeed;
+    this.controls.enableDamping = enableDamping;
+
+    this.moveToRoom(startRoomNo);
+  }
 
   ngAfterViewInit(): void {
-    const badLogo = document.querySelector('iframe .top-gui') as HTMLDivElement;
-    badLogo.style.display = 'none';
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    this.renderer2.appendChild(this.container.nativeElement, this.renderer.domElement);
+    this.animate();
   }
 
-  nextRoom(): void {
-    if (this.currentRoom === 3) {
-      this.currentRoom = 1;
+  @HostListener('mousewheel', ['$event'])
+  onMouseWheel({ deltaY }: WheelEvent): void {
+    const newZoomValue = this.currentZoom + -deltaY / 5000;
+    if (newZoomValue < minZoom || newZoomValue > maxZoom) {
+      this.currentZoom = this.currentZoom;
     } else {
-      this.currentRoom += 1;
+      this.currentZoom = newZoomValue;
     }
   }
 
-  previousRoom(): void {
-    if (this.currentRoom === 1) {
-      this.currentRoom = 3;
-    } else {
-      this.currentRoom -= 1;
+  moveToRoom(roomNo: number): void {
+    this.generatingScene = true;
+    this.scene.remove(this.mesh);
+
+    if (roomNo === 0) {
+      roomNo = maxRooms;
     }
+
+    if (roomNo > maxRooms) {
+      roomNo = 0;
+    }
+
+    this.currentRoomNo = roomNo;
+    setTimeout(() => {
+      this.loader.load(`${baseAssetsPath}/${roomNo}.jpg`, (texture) => {
+        const material = new THREE.MeshBasicMaterial({ map: texture });
+        this.mesh = new THREE.Mesh(this.geometry, material);
+        this.scene.add(this.mesh);
+      });
+
+      this.generatingScene = false;
+    }, 400);
+  }
+
+  animate() {
+    window.requestAnimationFrame(() => this.animate());
+
+    this.controls.update();
+    this.camera.zoom = this.currentZoom;
+    this.camera.updateProjectionMatrix();
+
+    this.renderer.render(this.scene, this.camera);
   }
 }
--- src/app/modules/virtual-tour/components/virtual-tour/virtual-tour.config.ts
+++ src/app/modules/virtual-tour/components/virtual-tour/virtual-tour.config.ts
@@ -1,2 +1,38 @@
+import { Asset } from './virtua-tour.model';
+
 export { appName } from '../../../../app.config';
-export const startRoomNo = 1;
+export const rotateSpeed: number = 0.1;
+export const enableDamping: boolean = true;
+export const startZoom: number = 1;
+export const maxZoom: number = 1.5;
+export const minZoom: number = 0.5;
+export const startRoomNo: number = 1;
+export const maxRooms: number = 5;
+export const baseAssetsPath: string = 'assets/imgs/360';
+export const assets: Asset[] = [
+  {
+    src: `${baseAssetsPath}/1.jpg`,
+    alt: 'Room 1',
+    roomNo: 1,
+  },
+  {
+    src: `${baseAssetsPath}/2.jpg`,
+    alt: 'Room 2',
+    roomNo: 2,
+  },
+  {
+    src: `${baseAssetsPath}/3.jpg`,
+    alt: 'Room 3',
+    roomNo: 3,
+  },
+  {
+    src: `${baseAssetsPath}/4.jpg`,
+    alt: 'Room 4',
+    roomNo: 4,
+  },
+  {
+    src: `${baseAssetsPath}/5.jpg`,
+    alt: 'Room 5',
+    roomNo: 5,
+  },
+];
